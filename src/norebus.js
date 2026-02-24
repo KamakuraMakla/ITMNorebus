e88fa17d0919f69b5c90ab899e479ac2559a4062
sakinkomaru <sakiiwai0729@gmail.com>
2026-02-24 22:13:52 +0900

    viewmore

--- src/norebus.js
+++ src/norebus.js
@@ -1,10 +1,19 @@
-$(function() {
-  $('.hamburger').click(function() {
-    // メニューの開閉
-    $('.globalnav').toggleClass('open');
-    $('.button').toggleClass('active');
-
-    // ハンバーガーボタンのアクティブクラスを切り替えて三本線をバツにする
-    $(this).toggleClass('active');
-  });
+$(document).ready(function() {
+    $('.js-view-more').on('click', function() {
+        // 同じ階層にある .more-content を探す
+        const $moreContent = $(this).prev('.more-content');
+        
+        // テキストの開閉をスライドで行う
+        $moreContent.slideToggle(400);
+        
+        // ボタンに「開いている」状態のクラスをつけ外しする（矢印回転用）
+        $(this).toggleClass('is-open');
+        
+        // ボタンのテキストを切り替える場合（任意）
+        if ($(this).hasClass('is-open')) {
+            $(this).html('<span class="arrow-icon">∧</span> CLOSE');
+        } else {
+            $(this).html('<span class="arrow-icon">∨</span> VIEW MORE');
+        }
+    });
 });
