07109bb16ee67be4d3cc65daf9d4d56553286a88
makla <doburoi0114@gmail.com>
2026-05-14 03:09:32 +0900

    tools の画像表示の仕方を変更

--- src/tools.js
+++ src/tools.js
@@ -6,17 +6,24 @@ async function sendImage(file, url) {
   const formData = new FormData();
   formData.append("image", file);
 
-  const res = await fetch(url, {
-    method: "POST",
-    body: formData
-  });
+ const res = await fetch("https://kyotonorebus.kamakuramakla.org/norebus.php?action=count", {
+  method: "POST",
+  body: formData,
+});
 
-  const data = await res.json();
-  console.log("person count:", data.count);
+const json = await res.json();
 
-    // 画像表示
-  const imgEl = document.getElementById("resultImg");
-  imgEl.src = "data:image/jpeg;base64," + data.image_b64_jpg;
+const count = json.data?.count;
+const imageBase64 = json.data?.image_b64_jpg;
+
+console.log("person count:", count);
+
+document.querySelector("#person-count").textContent = count ?? 0;
+
+if (imageBase64) {
+  document.querySelector("#result-image").src =
+    `data:image/jpeg;base64,${imageBase64}`;
+}
 
   document.getElementById("queue").value = data.count;
 }
